cf1ebc1a7788b1d15da9a77364d0014d54abbb85
wang-junjian <wang-junjian@qq.com>
2018-08-18 00:35:27 +0800

    采集www.game2.cn验证码

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Scrapy Hub
 
 * [采集乔丹www.qiaodan.com商品图片](qiaodan/)
+* [采集www.game2.cn验证码](verification_code/)
---
+++ verification_code/game2/game2/spiders/get_verification_code.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import os
+from datetime import datetime
+
+
+class GetVerificationCodeSpider(scrapy.Spider):
+    name = 'get_verification_code'
+    allowed_domains = ['game2.cn']
+    start_urls = ['http://game2.cn/']
+    verification_code_url = 'http://www.game2.cn/verifyCode.php'
+    images_dir = 'images/'
+    download_times = 100
+
+    def start_requests(self):
+        for _ in range(self.download_times):
+            yield scrapy.Request(self.verification_code_url,
+                                 dont_filter=True,)
+
+    def parse(self, response):
+        # if response.status == 200:
+
+        if not os.path.exists(self.images_dir):
+            os.makedirs(self.images_dir)
+
+        image_filename = self.get_image_filename()
+        with open(image_filename, "wb") as f:
+            f.write(response.body)
+
+    def get_image_filename(self):
+        name = str(int(datetime.now().timestamp()*1000000))
+        filename = '{}/{}{}'.format(self.images_dir, name, '.png')
+        return filename
